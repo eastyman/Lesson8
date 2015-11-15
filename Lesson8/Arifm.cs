417d34ce89d99b6ef9bab48dbd79332e0092aa56
eastyman <bra4o.il@gmail.com>
2015-11-15 14:38:57 +0200

    1

---
+++ Lesson8/Arifm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    static class Arifm
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Lesson8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int res = Arifm.Sum(1,10);
+            //Console.WriteLine(res);
+            //res = Arifm.Mul(10, 15);
+            //Console.WriteLine(res);
+        }
+
+      
+  
+    }
+}
